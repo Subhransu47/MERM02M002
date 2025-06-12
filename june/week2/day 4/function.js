9e334920236a983f5487c5df9b26eb15cbd68288
Subhransu47 <subhransupal2024@gift.edu.in>
2025-06-12 12:51:48 +0530

    ok

---
+++ june/week2/day 4/function.js
@@ -0,0 +1,120 @@
+//this is function declaration
+function print(){
+    console.log("this is my fuction")
+}
+
+//this is function invocation/calling
+print()
+//console.log(print)
+
+let printQlith=function(){
+    console.log("thios is my expression function")
+}
+
+printQlith()
+//console.log(printQlith)
+
+//function with parameter
+let add=function(a,b){
+    return a+b;
+}
+
+let sum=add(10,20)
+console.log(sum)
+//console.log(add)
+
+let sub=()=>{
+    console.log("this is my arrow function")
+}
+sub()
+//console.log(sub)
+
+//higher order function
+function hof(a,b,task){
+    task(a,b)
+}
+
+let addNum=hof(10,20,(x,y)=>{
+    return x+y;
+})
+console.log(addNum)
+
+
+// create a function using the function keyword that takes a string as an argument and return the number of vowels in the string
+// let newString=prompt("enter some string :")
+// function checkVowel(str){
+
+//     let newStr=str.toLowerCase();
+    
+//     let count=0
+// //     for(let e of newStr){
+
+// //         if(e=== "a"|| e=== "e" || e==="i" || e==="o" || e==="u"){
+// //             count++
+// //         }
+// // }
+// // return count
+// // }
+// // console.log("No of vowels is :", checkVowel(newString))
+
+// for (let e of newStr) {
+//         if ("aeiou".includes(e)) {
+//             count++
+//         }
+//     }
+//     return count
+// }
+
+// console.log("Number of vowels is:", checkVowel(newString));
+
+
+let arr=[1,2,3,4,5,6,7,8]
+let count=0
+arr.forEach((v,idx,ar)=>{
+    if (v%2==0) {
+        count++
+    }
+})
+console.log(count)
+
+let strArr=["pagal","dasu","bakadi","choro","subhransu"]
+let countStartWithS=0
+
+strArr.forEach((v)=>{
+    if (v.startsWith("s")) {
+        countStartWithS++;
+    }
+})
+console.log("ans is :",countStartWithS)
+
+//this is map function
+let numArr=[1,2,3,4,5]
+
+let modifiedArr=numArr.map((v)=>{
+    return v+1
+})
+console.log(modifiedArr)
+
+
+//filter method
+let filterData=numArr.filter((v)=>{
+    //with if condition
+    // if (v%2===0) {
+    //     return v
+    // }
+
+    return v%2===0
+})
+console.log(filterData)
+
+//this is reduce method
+let sumofNumber=numArr.reduce((accu,curr)=>{
+    return accu+curr;
+})
+//console.log("sum is :",sumofNumber)
+
+let sumNu=0
+for(let e of numArr){
+    sumNu+=e;
+}
+console.log(sumNu)
